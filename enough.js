905040664ea0fe9683a606d7e676c81002eccc27
Wys <noahgaming951@gmail.com>
2023-05-02 12:57:14 +0200

    Pushing initial code

--- enough.js
+++ enough.js
@@ -1,1 +1,19 @@
-console.log("Hello, VSCODE!")
+// Define constants
+const { Select } = require('enquirer');
+const chalk = require("chalk");
+
+// Changing variables
+let lang = require("./lang/en.json")
+
+// Stored prompts
+const startMode_p = {
+    message: 'Select start mode',
+    choices: ['Regular', 'Debug']
+}
+
+// Welcome prompt
+console.clear();
+console.log(chalk.bold("👋 Welcome to da-readme-generator\n"));
+
+new Select(startMode_p).run().then(answer => console.log('Answer:', answer)).catch(console.error);
+
